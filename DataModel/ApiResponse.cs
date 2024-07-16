6d400ea24f78495792c1bcf44ce21a1d391da8cd
Mojo <mojo@gmail.com>
2024-07-16 23:25:30 +0100

    Request/Response data model added

---
+++ DataModel/ApiResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace Bits.Api.Tests.DataModel
+{
+    public class ApiResponse
+    {
+        public string status { get; set; }
+        public Data data {  get; set; }
+        public ErrorResponse error {  get; set; }   
+    }
+
+    public class Data
+    {
+        public string userId {  get; set; }
+        public string status { get; set; }
+        public string title { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string dateOfBirth { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int rating { get; set; }
+    }
+
+    public class ErrorResponse
+    {
+        public string errorType { get; set; }
+        public string errorMessage { get; set; }
+    }
+}
---
+++ DataModel/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bits.Api.Tests.DataModel
+{
+    public class User
+    {
+        public string title {  get; set; }  
+        public string firstName { get; set; }
+        public string lastName { get; set; }    
+        public string dateOfBirth {  get; set; }    
+        public string email { get; set; }   
+        public string password { get; set; }    
+        public int rating { get; set; }
+    }
+}
