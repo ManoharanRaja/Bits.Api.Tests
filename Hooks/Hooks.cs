57e8b97310f00fcdb1b6b0f2c68fa1cbb90ee434
Mojo <mojo@gmail.com>
2024-07-20 13:58:30 +0100

    ExtentReport implementation
    Signed-off

--- Hooks/Hooks.cs
+++ Hooks/Hooks.cs
@@ -1,50 +1,74 @@
-﻿using TechTalk.SpecFlow;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using System.Reflection;
+using TechTalk.SpecFlow;
 
 namespace Bits.Api.Tests.Hooks
 {
     [Binding]
     public sealed class Hooks
     {
-        private readonly ScenarioContext _scenarioContext;
-        public Hooks(ScenarioContext scenarioContext)
-        {
-            _scenarioContext = scenarioContext; 
-        }
+        
+        private static ExtentTest featureName;
+        private static ExtentTest scenario;
+        private static ExtentReports extent;
+
 
         [BeforeTestRun]
         public static void BeforeTestRun()
         {
-             
+            var htmlReporter = new ExtentHtmlReporter(Directory.GetCurrentDirectory());
+            htmlReporter.Config.Theme = Theme.Standard;
+            htmlReporter.Config.ReportName = "API Test Summary Report";
+            htmlReporter.Start();
+
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
         }
 
         [BeforeScenario]
-        public void BeforeScenario()
+        public static void BeforeScenario(ScenarioContext sc)
         {
-
-        }
-
-        [BeforeStep]
-        public void BeforeStep()
-        {
-
+            scenario = extent.CreateTest(sc.ScenarioInfo.Title);
         }
 
         [AfterStep]
-        public void AfterStep()
+        public void AfterStep(ScenarioContext sc)
         {
-
-        }
-
-        [AfterScenario]
-        public void AfterScenario()
-        {
-
+            var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+            PropertyInfo pInfo = typeof(ScenarioContext).GetProperty("ScenarioExecutionStatus", BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo getter = pInfo.GetGetMethod(nonPublic: true);
+            object TestResult = getter.Invoke(sc, null);
+            if (sc.TestError == null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text);
+            }
+            if (sc.TestError != null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+                if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+                if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+                if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+            }
         }
         
         [AfterTestRun]
         public static void AfterTestRun()
         {
-
+            extent.Flush();
         }
 
     }
